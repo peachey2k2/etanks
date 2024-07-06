84052fd093c43cb163e6ff791a71d0de24486193
peachey2k2 <ogzpolat@gmail.com>
2024-07-06 11:43:01 +0300

    colors and selection

--- etanks.lua
+++ etanks.lua
@@ -1,10 +1,10 @@
 -- etanks.lua, made by peachey2k2
 -- under MIT license.
 
-fzy = require("lib/fzy")
-filename = "data/etankdata.txt"
+local fzy = require("lib/fzy")
+local filename = "data/etankdata.txt"
 
-channels = {
+local channels = {
     "white",
     "orange",
     "magenta",
@@ -62,6 +62,10 @@ end
 function colorToChar(colorIndexes)
     return string.format("%x%x%x", colorIndexes[1], colorIndexes[2], colorIndexes[3])
 end
+
+function sortFzyOutput(a, b)
+    return a[3] > b[3]
+end
         
 
 -- actual program starts here
@@ -95,18 +99,44 @@ io.close(file)
 
 local width, height = term.getSize()
 local input = ""
-local fluids = {}
+local fluidStr = {}
+local fluidData = {}
+local result = {}
+local selection = 1
 
 local debugText = "" -- for testing purposes
 
 function updateScreen()
-    local result = fzy.filter(input, fluids, false)
+    -- get the fuzzy search results
+    result = fzy.filter(input, fluidStr, false)
+    
+    -- sort them
+    table.sort(result, sortFzyOutput)
+    
     term.clear()
     term.setBackgroundColor(colors.black)
     for i = 1, height-1 do
         if result[i] == nil then break end
         term.setCursorPos(1, height-i-2)
-        term.write(fluids[result[i][1]])
+        
+        if i == selection then
+            term.setBackgroundColor(colors.white)
+            term.setTextColor(colors.black)
+        end
+        local cur = fluidData[ result[i][1] ]
+        term.write(cur[1])
+        term.setBackgroundColor(colors.black)
+        term.setTextColor(colors.white)
+        
+        term.write(" (")
+        for i = 2, 6, 2 do
+            if i ~= 2 then term.write(" - ") end
+            term.setTextColor(colors.fromBlit(cur[i]))
+            term. write(cur[i+1])
+            term.setTextColor(colors.white)
+        end
+        term.write(")")
+        --term.write(" (" .. result[i][3] .. ")")
     end
     term.setCursorPos(1, height-1)
     term.write(input)
@@ -119,9 +149,9 @@ end
 for line in io.lines(filename) do
     local colorChars = string.sub(line, 1, 3)
     local c = charToColor(colorChars)
-    local colorStr = channels[c[1]] .. " - " .. channels[c[2]] .. " - " .. channels[c[3]]
-    fluids[#fluids+1] = string.sub(line, 4, #line) .. " (" .. colorStr .. ")"
-    print(#fluids)
+    local fluid = string.sub(line, 4, #line)
+    fluidStr[#fluidStr+1] = fluid .. " (" .. channels[c[1]] .. " - " .. channels[c[2]] .. " - " .. channels[c[3]] .. ")"
+    fluidData[#fluidData+1] = {fluid, colorChars[1], channels[c[1]], colorChars[2], channels[c[2]], colorChars[3], channels[c[3]]}
 end
 
 updateScreen()
@@ -130,22 +160,26 @@ while true do
     local event, char, _ = os.pullEvent() -- events are yielded, no need for a sleep.
     if event == "char" then
         input = input .. char
+        selection = 1
     end
     if event == "key" then
         --debugText = " " .. keys.getName(char)
         if char == keys.backspace then
             input = #input > 0 and string.sub(input, 1, #input - 1) or input
-        end
-        if char == keys.enter then
+            selection = 1
+        elseif char == keys.enter then
             --
-        end
-        if char == keys.leftCtrl or char == keys.rightCtrl then
+        elseif char == keys.leftCtrl or char == keys.rightCtrl then
             term.clear()
             term.setCursorPos(1, 1)
             return
-        end
-        if char == keys.delete then
+        elseif char == keys.delete then
             input = ""
+            selection = 1
+        elseif char == keys.up and selection < #result then
+            selection = selection + 1
+        elseif char == keys.down and selection > 1 then
+            selection = selection - 1
         end
     end
     updateScreen()
